ca2928f7dcd9a3b4c488195f7192a3aa8d9e97bb
computergrrl <44240149+computergrrl@users.noreply.github.com>
2019-02-06 22:29:59 -0800

    Returned ListingBasicStatusTest to previous state

--- tests/ListingBasicStatusTest.php
+++ tests/ListingBasicStatusTest.php
@@ -6,44 +6,24 @@ use PHPUnit\Framework\TestCase;
 
 class ListingBasicStatusTest extends TestCase
 {
-  public $data = [
-      'id' => '222',
-      'title' => 'Amber is a rock star',
-      'website' => 'www.example-website.com',
-      'email' => 'anonymous@email.com',
-      'twitter' => '@RandomPerson',
-  ];
 
-//   public function testStatus()
-//   {
-//
-// /*create an array to pass to ListingBasic Object*/
-//             $data = [
-//                'id' => '18',
-//             'title' => 'The Test'
-//             ];
-//
-//     $listing = new ListingBasic($data);
-// //call the getStatus method
-//       $status = $listing->getStatus();
-//
-// //does getStatus method return value equal to 'basic' ?
-//       $this->assertEquals($status , 'basic');
-//
-//   }
-
-  public function testTwitter()
+  public function testStatus()
   {
 
 /*create an array to pass to ListingBasic Object*/
+            $data = [
+               'id' => '222',
+            'title' => 'The Test'
+            ];
 
     $listing = new ListingBasic($data);
 //call the getStatus method
-      $status = $listing->getTwitter();
+      $status = $listing->getStatus();
 
 //does getStatus method return value equal to 'basic' ?
       $this->assertEquals($status , 'basic');
 
   }
+
 
 }
